518a83a9f80358ac78e21dc3abf3c316bf9b0536
corentin-numericks <corentin@numericks.com>
2021-03-02 17:56:58 +0100

    hooks tuto with fetch

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,118 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
+ 
+function useIncrement(init, step){
+  const [count, setCount]=useState(init)
+
+  const increment =() => {
+    setCount(c => c + step)
+  }
+
+  return [count, increment]
+}
+
+function useAutoincrement(init, step){
+  const [count, increment]= useIncrement(init ,step)
+
+  useEffect(() => {
+    const timer=
+    window.setInterval(() => {
+        increment()
+    }, 1000)
+
+    return function() {
+      clearInterval(timer)
+    }
+  }, [count])
+
+  return [count]
+}
+
+function Compteur(){
+  const [count, increment] = useIncrement(10, 1)
+  const [count2]= useAutoincrement(2, 2)
+
+    return <div>
+    <button onClick={increment}>Incrémenter {count}</button>
+    <button>Incrémenter {count2}</button>
+
+    </div>
+
+}
+
+function useToggle (initialValue){
+  const [value, setValue]=useState(initialValue)
+
+  const toggle =(e) => {
+    setValue( i => !i)
+  }
+
+  return [value, toggle]
+
+}
+
+function useFetch(url){
+  const [state, setState]= useState({
+    items: [],
+    loading: true
+  })
+
+  useEffect(() => {
+    (async () => {
+      const response= await fetch(url)
+      const responseData= await response.json()
+      if (response.ok){ 
+        setState({
+          items: responseData,
+          loading: false
+        })
+      } else {
+        alert(JSON.stringify(responseData))
+        setState(s => ({...s, loading: false}))
+      }
+    })()
+  }, [])
+
+    return [
+      state.loading,
+      state.items
+    ]
+}
+
+function PostTable(){
+  const [loading, items] = useFetch('https://jsonplaceholder.typicode.com/comments?_limit=10')
+  return <ul>
+    {items.map((i) => <li key={i.id}>Name: {i.name}, EMAIL: {i.email} , CONTENT: {i.body} </li>) }
+  </ul>
+
+}
+
+function TodoList (){
+  const [loading, items] = useFetch('https://jsonplaceholder.typicode.com/todos?_limit=80')
+
+  return <ul>
+    {items.map((t, key) => <li key={key}>{t.title}</li>) }
+  </ul>
+}
+
+function App(){
+
+  const [compteurVisible, toggleCompteur]= useToggle(true)
+
+    return <div className={styles.container}>
+      Afficher le compteur <input type="checkbox" onChange={toggleCompteur} checked={compteurVisible} />
+      <br />
+      {compteurVisible && <Compteur />}
+      <TodoList />
+      <PostTable />
+    </div>
+  
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <App />
   )
 }
+
